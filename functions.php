bb64b194a098750adefb4b518895fd5ec320fc3e
Javier Troya <javiertroya@gmail.com>
2025-03-31 21:27:31 +0200

    Working.

--- functions.php
+++ functions.php
@@ -78,4 +78,4 @@ add_filter('pre_http_request', function($a) {
 });
 
 require_once dirname(__FILE__) . '/vendor/autoload.php';
-require_once dirname(__FILE__) . '/config/application.php';
+//require_once dirname(__FILE__) . '/config/application.php';
